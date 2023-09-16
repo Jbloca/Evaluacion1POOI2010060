b6d8ddd7b627d048937596fdab2ab4d80e1c84ac
Jorge Balarezo <127998906+Jbloca@users.noreply.github.com>
2023-09-16 09:59:29 -0500

    Create ejercicio2.java

---
+++ ejercicio2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Jorge Balarezo";
+        String direccion = "Mariano cornejo 333";
+        String telefono = "987-654-321";
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+    }
+}
