eedb4f6aa5f14ad483a6a865227f94cd0defcebd
Jorge Balarezo <127998906+Jbloca@users.noreply.github.com>
2023-09-16 09:58:48 -0500

    Update ejercicio1.java

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,20 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        // Declarar las variables enteras x e y y asignarles valores
         int x = 144;
         int y = 999;
 
-        // Mostrar el valor de cada variable
         System.out.println("El valor de x es: " + x);
         System.out.println("El valor de y es: " + y);
 
-        // Realizar las operaciones matemáticas
         int suma = x + y;
         int resta = x - y;
         int division = x / y;
         int multiplicacion = x * y;
 
-        // Mostrar los resultados
         System.out.println("La suma de x e y es: " + suma);
         System.out.println("La resta de x y y es: " + resta);
         System.out.println("La división de x entre y es: " + division);
